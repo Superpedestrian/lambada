47a66eadee9f9925852c94000764180176fb59c6
Carson Gee <x@carsongee.com>
2016-10-21 14:57:05 -0400

    fixup! fixup! Added relative path when loading python modules for discovery

--- lambada/tests/test_common.py
+++ lambada/tests/test_common.py
@@ -34,9 +34,11 @@ class TestCommon(TestCase):
         self.assertTrue('test_lambada' in tune.dancers)
 
         # Now by folder
+        original_sys_path = sys.path[:]
         tune = common.get_lambada_class(os.path.dirname(path))
         self.assertIsNotNone(tune)
         self.assertTrue('test_lambada' in tune.dancers)
+        self.assertEqual(original_sys_path, sys.path)
 
         # Assert exception by specifying location that doesn't exist
         path = make_fixture_path('nodancers', 'nope.py')
